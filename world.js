1a65cfad61f8f13c5b018d4a965d945abe90d48f
Narika Hall <narikahall@gmail.com>
2022-11-16 02:27:06 -0500

    Updated Files

--- world.js
+++ world.js
@@ -1,27 +1,39 @@
 window.onload = function(){
-var btn = document.getElementById("lookup");
-var result= document.getElementById('result');
 
+    const btn = document.getElementById('lookup');
+    const result= document.getElementById('result');
 
-      
+    btn.addEventListener('click', function(ev){
+        ev.preventDefault();
+        console.log('work')
+        var country= document.getElementById('country').value; 
+        console.log(country);
+        var url=  `http://localhost:8888/info2180-lab5/world.php?country=` +country; 
+        console.log(url);
 
-btn.addEventListener('click', function(ev){
-    ev.preventDefault();
-    console.log('work')
-    var country= document.getElementById('country').value; 
-    console.log(country);
-    var url=  `http://localhost:8888/info2180-lab5/world.php?country=` +country; 
-    console.log(url);
-
-    fetch(url)
+        fetch(url)
             .then(response => response.text())
-            
+                
             .then(data =>{
                 console.log(data)
                 result.innerHTML= data;
-        })   
-        
+            })           
+    })
 
-})
+    btn.addEventListener('click', function(ev){
+        ev.preventDefault();
+        console.log('work')
+        var country= document.getElementById('country').value; 
+        console.log(country);
+        var url=  `http://localhost:8888/info2180-lab5/world.php?country=${country}&context=cities`; 
+        console.log(url);
 
+        fetch(url)
+            .then(response => response.text())
+                
+            .then(data =>{
+                console.log(data)
+                result.innerHTML= data;
+            })           
+    })
 }
--- world.php
+++ world.php
@@ -1,7 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 
-$host = 'localhost';
+$host = 'localhost:8888';
 $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
@@ -10,19 +10,39 @@ $dbname = 'world';
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 
 
-
 if ($_SERVER['REQUEST_METHOD'] == 'GET'){
+
+  
   if (isset($_GET['country']) && isset($_GET['context']) && !empty($_GET['country'])){
+  
     $country= ucwords(trim(filter_input($_GET['country'], FILTER_SANITIZE_STRING))) ;
-    $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
-    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);  
-  }
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC); 
+    $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'"); 
+    ?>
+
+    <table>
+      <tr>
+        <th> Country Name </th>
+        <th> Continent </th>
+        <th> Independence </th>
+        <th> Head of State </th>
+      </tr>
+      <tbody>
+        <?php foreach ($results as $row): ?>
+          <tr>
+            <td><?= $row['name']?></td>
+            <td><?= $row['continent']?></td>
+            <td><?= $row['independence_year']?></td>
+            <td><?= $row['head_of_state']?></td>
+        </tr>
+        <?php endforeach; ?>
+      </tbody>
+    </table>
+
+     
+  <?php}
 
 }
 
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+
